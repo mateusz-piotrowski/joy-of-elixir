705889faa8faa25c0fa53a0a875f18f04f178bb4
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2022-09-08 00:34:25 +0200

    Add 1-appeasing-the-masses.exs file

---
+++ 1-appeasing-the-masses.exs
@@ -0,0 +1,19 @@
+IO.puts("")
+IO.puts("# = = = = = = = = = = ")
+IO.puts("# Chapter 1")
+IO.puts("# Name: Appeasing the masses with code")
+IO.puts("# File: 1-appeasing-the-masses.exs")
+IO.puts("# = = = = = = = = = = ")
+IO.puts("")
+
+IO.puts("Exercise no. 1")
+result = 24 * 60
+IO.puts("Summary:")
+IO.puts(result)
+IO.puts("")
+
+IO.puts("Exercise no. 2")
+average = (4 + 8 + 15 + 16 + 23 + 42) / 6
+IO.puts("Average:")
+IO.puts(average)
+IO.puts("")
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,9 @@
 
 ## [0.0.2] (September 11 2022)
 
-**Updated :**
+**Added :**
 
-- README.md
+- 1-appeasing-the-masses.exs file
 
 ## [0.0.1] (March 11 2022)
 
